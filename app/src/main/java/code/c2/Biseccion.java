d4743ad26c4848233d55d36497b5e62785887859
Yael Molinero <y.molinero20@gmail.com>
2026-06-29 18:44:35 -0600

    Metodo biseccion completado

--- app/src/main/java/code/c2/Biseccion.java
+++ app/src/main/java/code/c2/Biseccion.java
@@ -7,43 +7,63 @@ import code.utils.Consola;
 
 public class Biseccion extends Base {
 
-  public double evaluar(FuncionDinamica f, double a, double b, double tolerancia) {
+  public double evaluar(FuncionDinamica f, double a, double b, double tolerancia, int iteracionesMax) {
     System.out.printf("Intervalo (%.0f, %.0f) -> a: %.0f, b: %.0f\n", a, b, a, b);
     System.out.printf("Error < %f%%\n\n", tolerancia);
     
-    double medio;
+    double fa = redondear(f.evaluar(a));
+    double fb = redondear(f.evaluar(b));
+    if (fa * fb >= 0) throw new Error("El intervalo no cumple con el método de bisección");
+    
+    double medio = 0;
+    double fc = 0;
+    double error = Double.MAX_VALUE;
     int i = 0;
 
     System.out.printf(
       Chalk.bold(Chalk.bgBlue(
-      "%-2s %-8s %-8s %-10s %-10s %-10s %-10s")),
-      "i", "a", "b", "f(a)", "f(b)", "f(c)", "E"
+      "%-2s %-8s %-8s %-8s %-10s %-10s %-10s %-10s")),
+      "i", "a", "b", "medio", "f(a)", "f(b)", "f(c)", "E"
     );
     System.out.println();
     
-    do {
-      medio = redondear((a + b) / 2);
+    while (error > tolerancia && i < iteracionesMax) {
       double tempA = a, tempB = b;
+      medio = redondear((a + b) / 2);
+
+      fa = redondear(f.evaluar(a));
+      fb = redondear(f.evaluar(b));
+      fc = redondear(f.evaluar(medio));
+
+      error = redondear((b - a) / 2.0);
       i++;
 
-      if (f.evaluar(medio) > 0) b = medio;
-      else a = medio;
+      // if (f.evaluar(medio) > 0) b = medio;
+      // else a = medio;
 
       System.out.printf(
-        "%-2d %-8f %-8f %-10f %-10f %-10f %-10f\n",
+        "%-2d %-8f %-8f %-8f %-10f %-10f %-10f %-10f\n",
         i,
         tempA,
         tempB,
-        f.evaluar(tempA),
-        f.evaluar(tempB),
-        f.evaluar(medio),
-        (tempB - tempA) / 2
+        medio,
+        fa,
+        fb,
+        fc,
+        error
       );
-      
-      System.out.printf(Chalk.purple("%f > %f | %b") + "\n", Math.abs(b - a), Math.abs(tolerancia),  Math.abs(b - a) > Math.abs(tolerancia));
-    } while (Math.abs(b - a) > Math.abs(tolerancia) && i < 16);
 
-    return redondear(Math.abs(b - a));
+      if (fc == 0.0) break;
+      if (fa * fc < 0) {
+        b = medio;
+        fb = fc;
+      } else {
+        a = medio;
+        fa = fc;
+      }
+    }
+
+    return medio;
   }
 
   public static void main(String[] args) {
@@ -51,12 +71,13 @@ public class Biseccion extends Base {
     Consola consola = new Consola();
 
     String funStr = consola.getString("Ingrese la Funcion", "e^(3x) - 4");
+    FuncionDinamica fun = new FuncionDinamica(funStr);
     double intervaloA = consola.getInteger("Ingrese un valor para Xa: ", 0);
     double intervaloB = consola.getInteger("Ingrese un valor para Xb: ", 1);
     double err = consola.getDouble("Ingresa la tolerancia (ej. 0.01): ", false);
+    int iteracionesMax = consola.getInteger("Iteraciones máximas", 16);
     
-    FuncionDinamica fun = new FuncionDinamica(funStr);
-    double result = new Biseccion().evaluar(fun, intervaloA, intervaloB, err);
+    double result = new Biseccion().evaluar(fun, intervaloA, intervaloB, err, iteracionesMax);
 
     System.out.println();
     System.out.printf(Chalk.bgGreen("La interseccion es: %f") + "\n", result);
--- app/src/main/java/code/utils/FuncionDinamica.java
+++ app/src/main/java/code/utils/FuncionDinamica.java
@@ -1,14 +1,22 @@
 package code.utils;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
+import net.objecthunter.exp4j.tokenizer.UnknownFunctionOrVariableException;
 
 public class FuncionDinamica {
   private Expression fun;
 
   public FuncionDinamica(String expression) {
-    this.fun = new ExpressionBuilder(expression)
-      .variable("x")
-      .build();
+    try {
+      this.fun = new ExpressionBuilder(expression)
+        .variable("x")
+        .build();
+
+      fun.validate();
+      
+    } catch (UnknownFunctionOrVariableException e) {
+      throw new Error("Ingrese una función valida.");
+    }
   }
 
   public double evaluar(double x) {
